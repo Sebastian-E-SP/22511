a0ff112a2265dbcf10fda02841830c309755189e
MarianoTami <mariano13age@gmail.com>
2022-10-17 09:17:40 -0300

    mon

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
 });
+
+// Contact form
+
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('errror')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null) ( {
+        messages.push('Name is required')
+    }
+
+    if(password.length <=6){
+        messages.push('Password must be longer than 6 characters')
+    }
+
+    if (messages.length > 0) {
+       e.preventDefault()
+       errorElement.innerText = messages.join(', ')
+
+    }
+
+    
+})
