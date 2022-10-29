9d88d96ad3c6b3285aa06de2b00b75008c8154b1
Alejandro <63213761+venerado2001@users.noreply.github.com>
2022-10-29 16:13:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
+const iconoMenu = document.querySelector('#icono-menu');
+const menu = document.querySelector('#menu2');
 
-// Contact form
+iconoMenu.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+});
 
-const name = document.getElementById('name')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('errror')
-
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) ( {
-        messages.push('Name is required')
-    }
-
-    if(password.length <=6){
-        messages.push('Password must be longer than 6 characters')
-    }
-
-    if (messages.length > 0) {
-       e.preventDefault()
-       errorElement.innerText = messages.join(', ')
-
-    }
-
-    
+iconoMenu.addEventListener('click', e =>{
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
 })
